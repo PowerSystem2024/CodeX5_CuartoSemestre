b7210e150dcba01341990ee742c1897a426157d4
Ana Castro <anacastronieto.x@gmail.com>
2025-08-30 14:23:11 -0300

    Añado actividades de java del dia 27/08

--- Java/Leccion01/Leccion01/Leccion01/estudiantes/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/Leccion01/Leccion01/Leccion01/estudiantes/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -8,7 +8,6 @@ public class EstudiantesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudiantesApplication.class, args);
-		System.out.println("hola");
 	}
 
 }
---
+++ Java/Leccion01/Leccion01/Leccion01/estudiantes/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
@@ -0,0 +1,11 @@
+package utn.estudiantes.servicio;
+
+import java.util.List;
+import utn.estudiantes.modelo.Estudiante;
+
+public interface IEstudianteServicio {
+    public List<Estudiante> listarEstudiantes();
+    public Estudiante buscarEstudiantePorId(Integer id);
+    public void guardarEstudiante(Estudiante estudiante);
+    public void eliminarEstudiante(Estudiante estudiante);
+}
