49d39f16c3d34ee2391145621f4cd87accbe12be
Ana Castro <anacastronieto.x@gmail.com>
2025-09-27 21:30:46 -0300

    Añado clase Java 24/09

--- Python/Python/Juego_Tecnicatura/assets_1/main.py
+++
@@ -1,6 +0,0 @@
-import pygame # type: ignore
-import sys
-import random
-import os
-from personaje import Personaje, Enemigo, Explosion
-from constantes import  SCREEN_WIDTH, SCREEN_HEIGHT, COLOR_LASER, ASSETS_PATH
