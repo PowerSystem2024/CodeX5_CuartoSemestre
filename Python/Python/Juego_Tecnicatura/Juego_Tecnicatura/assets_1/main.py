23175fe15358eb99b200c44bdc0b6081470622f2
Santino Salatino <salatinosantino1@gmail.com>
2025-09-30 17:51:26 -0300

    Agrego clase 08 de Python 29/09

--- Python/Python/Juego_Tecnicatura/Juego_Tecnicatura/assets_1/main.py
+++ Python/Python/Juego_Tecnicatura/Juego_Tecnicatura/assets_1/main.py
@@ -1,6 +1,86 @@
+from Juego_Tecnicatura import explosiones
+from Juego_Tecnicatura import personaje
 import pygame # type: ignore
 import sys
 import random
 import os
 from personaje import Personaje, Enemigo, Explosion
 from constantes import  SCREEN_WIDTH, SCREEN_HEIGHT, COLOR_LASER, ASSETS_PATH
+
+def mostrar_imagen_inicial(screen, imagen_path, duracion):
+    imagen = pygame.image.load(imagen_path).benvert()
+    imagen = pygame.transform.scale(image, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # Bucle para mostrar la imagen principal con una opacidad
+    alpha = 255 # transparencia inicial completa
+    Clock = pygame.time.Clock()
+
+    timepo_inicial = pygame.time.get_ticks()
+    tiempo_total = duracion # Duracion en milisegundos de (8000 milisegundos para 8 segundos)
+while pygame.time.get_ticks() - tiempo_inicial < tiempo_total:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit
+            sys.exit()
+
+    keys = pygame.key.get_pressed()
+    dx, dy = 0, 0
+    if keys[pygame.K_LEFT]:
+        dx = -5
+    if keys[pygame.K_RIGHT]:
+        dx = 5
+    if keys[pygame.K_UP]:
+        dy = -5
+    if keys[pygame.K_DOWN]:
+        dy = 5
+    
+    personaje.mover(dx, dy)
+
+    if keys[pygame.K_SPACE]:
+        personaje.lanzar_laser()
+        sonido_laser.play()
+
+    # Actualizar posicion de enmigos y manejar las colisiones
+    for enemigo in enemigos[:]:
+        enemigo.mover()
+        if enemigo.rect.top > SCREEN_HEIGHT:
+            enemigos.remove(enemigo)
+
+    # Verificar colision con el laser
+    for laser in personaje.lasers[:]:
+        if enemigo.rect.colliderect(laser.react):
+            explosiones.append(Explosiones(enemigo.rect.centerx, enemigo.rect.centery))
+            enemigo.remove(enemigo) #elimina ala enemigo
+            personaje.lasers.remove(laser) #elimina el laser
+            sonido_explosion.play()
+            puntos += 10 #incrementa en puntaje 
+            break #sale del bulce
+
+    if enemigo.react.colliderect(personaje.shape):
+        if not personaje.recibir_dano():
+            running = False # Termina el juego si la energia llega a 0
+
+    # Generar enemigos de forma aleatoria
+    if random.randint(1, 10) < 2:
+        x = random.randint(0, SCREEN_WIDTH - 50) # nos asegura de tener al enemigo dentro de la pantalla
+        enemigo = Enemigo(x, 0)
+        enemigo.append(enemigo)
+
+    # Actualizar explosiones
+    explosion = [ explosion for explosion in explosiones if explosion.actualizar()]
+
+    # Actualizar el fondo cada 250 puntos
+    if puntos > 0 and puntos % 250 == 0:
+        if fondo_actual == fondo2:
+            fondo_actual = fondo3
+        else:
+            fondo_actual = fondo2
+            puntos += 10 # Aumenta puntos y cambia el fondo
+
+    # Dibujar el fondo y objetos en la pantalla 
+    screen.blit(fondo_actual, (0, 0))
+    personajes.dibujar(screen)
+    for enemigo in enemigos:
+        enemigo.dibujar(screen)
+    for explosion in explosiones:
+        explosion.dibujar(screen)
--- Python/Python/Juego_Tecnicatura/Juego_Tecnicatura/personaje.py
+++ Python/Python/Juego_Tecnicatura/Juego_Tecnicatura/personaje.py
@@ -1,2 +1,84 @@
 import pygame
-import os
+import os # proporcina funcionalidades para interactuar con el sistema
+from constantes import ASSETS_PATH
+
+class Persoanje:
+    def __init__(self, x, y):
+        #Construye la ruta a la imagen del personaje
+        self.image = pygame.image.load(os.path.join(ASSETS_PATH, "images", "personaje1.png"))
+        self.image = pygame.transform.scale(self.image, (95, 95))
+        self.shape = self.image.get_rect(center=(x,y))
+        self.laser = []
+        self.energia = 100 #Barra de energia
+
+    def mover(self, dx, dy):
+        self.shape.x += dx
+        self.shape.y += dy
+
+    def lanzar_laser(self):
+        laser = laser(self.shape.centerx, self.shape.top)
+        self.lasers.append(laser)
+
+    def recibir_dano(self):
+        self.energia -= 10
+        if self.energia <= 0:
+            self.energia <= 0
+            return False
+        return True
+    
+    def dibujar(self, screen):
+        screen.blit(self.image, self.shape.topleft)
+        for laser in self.lasers:
+            laser.dibujar(screen)
+            laser.mover()
+
+    #Dibujamos la barra de energia
+    pygame.draw.rect(screen(255, 0, 0), (10, 10, 100, 10))#barra de fondo
+    pygame.draw.rect(screen(0, 255, 0), (10, 10, self.energia, 10))
+
+
+
+    class Enemigo:
+        def __init__(self, x, y):
+            self.image = pygame.image.load(os.path.join(ASSETS_PATH, "images", "enemigo1_png"))
+            self.image = pygame.transform.scale(self.image, (80, 90))
+            self.image = self.image.get_rect(Topleft=(x, y))
+
+        def mover(self):
+            screen.blit(self.image, self.rect.topleft)
+
+    class Laser:
+        def __init__(self, x, y):
+            self.image = pygame.image.load(os.path.join(ASSETS_PATH, "image", "laser.png"))
+            self.rect = self.image.get_rect(center=(x, y))
+
+        def mover(self):
+            self.rect.y -= 5
+
+        def dibujar(self, screen):
+            screen.blit(self.image, self.rect.topleft)
+
+class Explosion:
+    def __init__(self, x, y):
+        self.images = [pygame.image.load(os.path.join(ASSETS_PATH, "images", f'regularExplosion0{i:2}.png'))
+        for i in range(9)]
+        self.index = 0 #indice de la animacion
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect(center=(x, y))
+        self.frame_rate = 0 #contador de los franes de la animacion
+        self.max_franes = 20 #franes por imagees
+    
+    def actualizar(self):
+        #actualiza la animacion
+        self.frame_rate += 1
+        if self.frame_rate >= self.max_franes:
+            self.index += 1
+            if self.index >= len(self.images):
+                return False #retorna en falso que aqui termina la animacion que va air de 0 -> 9
+            self.image = self.images[self.index]
+            return True 
+        
+    def dibujar(self, screen):
+        #dibuja la imagen en la pantalla
+        screen.blit(self.image, self.rect.topleft)
+        
