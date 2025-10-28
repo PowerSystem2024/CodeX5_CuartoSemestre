1cc3e2876d2d50645a9d90928301042df41ca371
leopa18 <leonelpalominos7@gmail.com>
2025-10-28 18:50:52 -0300

    Añado tienda libros completo y en funcionamiento

---
+++ Java/tiendaLibros/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/repositorio/LibroRepositorio.java
@@ -0,0 +1,9 @@
+package utn.tienda_libros.repositorio;
+import utn.tienda_libros.modelo.Libro;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface LibroRepositorio extends JpaRepository<Libro, Integer> {
+}
